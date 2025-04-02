866344ef3f7310c176ecf101a76efaa8cea03e64
Qwazzi2 <rodionplukci65@gmail.com>
2025-04-02 20:03:01 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, Render!'
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    data = request.json
+    response = requests.post('http://bot_endpoint/send_message', json=data)
+    return jsonify(response.json())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
